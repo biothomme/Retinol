20514df1e45f02a030ecb9df969df817b71d701f
biothomme <55461592+biothomme@users.noreply.github.com>
2021-04-01 11:00:05 +0200

    moved init inside

--- bumbleview/__init__.py
+++ bumbleview/__init__.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# version
+__version__ = "1.0.0"
+
+# run
 import convwale
 print("Welcome to the bumbleview package. Here you can covnert emission spectra to its reaction in an insect eye.")
-
