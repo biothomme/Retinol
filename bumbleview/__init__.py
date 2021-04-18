59fffb01e13479625f7f397f63eae14aad063466
biothomme <55461592+biothomme@users.noreply.github.com>
2021-04-18 16:23:55 +0200

    0.1.5

--- bumbleview/__init__.py
+++ bumbleview/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # version
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 # run
 print("Welcome to the bumbleview package. Here you can convert emission spectra to its reaction in an insect eye.")
